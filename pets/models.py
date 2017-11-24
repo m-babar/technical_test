4d0da17843c14f78ab12d397370146276120353d
Madhav <madhavbabar.21@gmail.com>
2017-11-24 22:29:17 +0530

    created model for pet and added migrations

--- pets/models.py
+++ pets/models.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Pet(models.Model):
+    PET_TYPE = (
+        ('D', 'Dog'),
+        ('C', 'Cat'),
+    )
+    
+    name = models.CharField(max_length=100, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    pet_type = models.CharField(max_length=1, choices=PET_TYPE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return "%s :: %s" % (self.name, self.pet_type)
